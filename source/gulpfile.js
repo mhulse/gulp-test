9e018199df3efe986ed77e43f0adda88ef968bb8
Micky Hulse <mickyhulse@gmail.com>
2014-07-01 23:08:20 -0700

    The start of my banners.

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -11,6 +11,7 @@
 
 // Load plugins
 var gulp = require('gulp'),
+	pkg = require('./package.json'),
 	sass = require('gulp-ruby-sass'),
 	autoprefixer = require('gulp-autoprefixer'),
 	minifycss = require('gulp-minify-css'),
@@ -18,7 +19,28 @@ var gulp = require('gulp'),
 	rename = require('gulp-rename'),
 	rimraf = require('gulp-rimraf'),
 	notify = require('gulp-notify'),
-	livereload = require('gulp-livereload'); 
+	livereload = require('gulp-livereload'),
+	header = require('gulp-header'),
+	banner = {
+		'short' : '/*! ' +
+		          '<%= pkg.title || pkg.name %>' +
+		          '<%= pkg.version ? " v" + pkg.version : "" %>' +
+		          '<%= pkg.licenses ? " | " + _.pluck(pkg.licenses, "type").join(", ") : "" %>' +
+		          '<%= pkg.homepage ? " | " + pkg.homepage : "" %>' +
+		          ' */',
+		'long' : '/**\n' +
+		         ' * <%= pkg.title || pkg.name %>\n' +
+		         '<%= pkg.description ? " * " + pkg.description + "\\n" : "" %>' +
+		         ' *\n' +
+		         '<%= pkg.author.name ? " * @author " + pkg.author.name + "\\n" : "" %>' +
+		         '<%= pkg.author.url ? " * @link " + pkg.author.url + "\\n" : "" %>' +
+		         '<%= pkg.homepage ? " * @docs " + pkg.homepage + "\\n" : "" %>' +
+		         ' * @copyright Copyright (c) <%= year %> <%= pkg.author.name %>.\n' +
+		         '<%= pkg.licenses ? " * @license Released under the " + _.pluck(pkg.licenses, "type").join(", ") + ".\\n" : "" %>' +
+		         '<%= pkg.version ? " * @version " + pkg.version + "\\n" : "" %>' +
+		         ' * @date <%= today %>\n' +
+		         ' */\n\n',
+	};
 
 // Styles
 gulp.task('styles', function() {
@@ -29,9 +51,19 @@ gulp.task('styles', function() {
 			style : 'expanded',
 		}))
 		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(header(banner.long, {
+			pkg: pkg,
+			today: function () {
+				return new Date().toISOString(); // yyyy/mm/dd
+			},
+			year: function () {
+				return new Date().toISOString(); // yyyy
+			}
+		}))
 		.pipe(gulp.dest('../dev/styles'))
 		.pipe(rename({ suffix: '.min', }))
 		.pipe(minifycss())
+		.pipe(header(banner.short, { pkg : pkg } ))
 		.pipe(gulp.dest('../dev/styles'))
 		.pipe(notify({ message: 'Styles task complete', }));
 	
